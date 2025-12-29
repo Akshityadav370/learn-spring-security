f9efe77245c072d5f44e5531e49eb6b2307e7377
Akshit <akshit07032001@gmail.com>
2025-12-29 14:20:27 +0530

    add: Role based authorization

--- src/main/java/com/learn_spring_security/spring_security/config/WebSecurityConfig.java
+++ src/main/java/com/learn_spring_security/spring_security/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import com.learn_spring_security.spring_security.handlers.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -19,6 +20,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static com.learn_spring_security.spring_security.entity.enums.Role.ADMIN;
+import static com.learn_spring_security.spring_security.entity.enums.Role.CREATOR;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -27,6 +31,10 @@ public class WebSecurityConfig {
     private final LoggingFilter loggingFilter;
     private final OAuth2SuccessHandler oAuth2SuccessHandler;
 
+    private static final String[] publicRoutes = {
+            "/error", "/auth/**", "/home.html"
+    };
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
@@ -34,8 +42,10 @@ public class WebSecurityConfig {
                 .sessionManagement(sessionConfig -> sessionConfig
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/posts", "/error", "/auth/**", "/home.html").permitAll()
-//                        .requestMatchers("/posts/**").hasAnyRole("ADMIN")
+                        .requestMatchers(publicRoutes).permitAll()
+                        .requestMatchers(HttpMethod.GET, "/posts/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/posts/**")
+                        .hasAnyRole(ADMIN.name(), CREATOR.name())
                         .anyRequest().authenticated())
                 .addFilterBefore(loggingFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
---
+++ src/main/java/com/learn_spring_security/spring_security/entity/enums/Role.java
@@ -0,0 +1,7 @@
+package com.learn_spring_security.spring_security.entity.enums;
+
+public enum Role {
+    USER,
+    CREATOR,
+    ADMIN
+}
